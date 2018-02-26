0503779f7cc5febfe25086f2d187c509315038e5
Jeremy Frank <jeremyjfrank@gmail.com>
2018-02-26 13:12:50 -0500

    Update copy and box padding on info page

--- src/pages/info.js
+++ src/pages/info.js
@@ -12,24 +12,15 @@ const InfoPage = () => (
   <div styleName="container">
     <h1 className="screenreader-only">Additional Information</h1>
 
-    <div styleName="intro">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
-        voluptatem similique dolores, hic, incidunt non saepe sit doloribus
-        labore nostrum laborum ea illum, ab ullam debitis animi veniam
-        mollitia suscipit?
-      </p>
-    </div>
-
     <ul styleName="grid">
       <li styleName="box">
-        <img src={backpackIcon} alt="" styleName="icon" />
-        <span>CARNE y ARENA is a virtual reality work that is highly immersive and can feel extremely realistic at times.</span>
+        <img src={compassIcon} alt="" styleName="icon" />
+        <strong>We recommend that you arrive at least 30 minutes prior to your appointment time.</strong>
       </li>
 
       <li styleName="box">
-        <img src={eyeIcon} alt="" styleName="icon" />
-        <span>CARNE y ARENA is not recommended for individuals with claustrophobia, heart conditions, back conditions, a history of seizures, epilepsy, and/or sensitivity to flashing lights.</span>
+        <img src={backpackIcon} alt="" styleName="icon" />
+        <span>CARNE y ARENA is a virtual reality work that is highly immersive and can feel extremely realistic including experiences with firearms.</span>
       </li>
 
       <li styleName="box">
@@ -39,17 +30,17 @@ const InfoPage = () => (
 
       <li styleName="box">
         <img src={chipIcon} alt="" styleName="icon" />
-        <span>Due to the complex technology employed, the installation may require unplanned maintenance; resulting in delays or cancellation. If it is not possible to reschedule a delayed or cancelled visit, a full refund will be issued.</span>
-      </li>
-
-      <li styleName="box">
-        <img src={compassIcon} alt="" styleName="icon" />
-        <span>We recommend that you arrive at least 30 minutes prior to your appointment time.</span>
+        <span>Due to the complex technology employed, the installation may require unplanned maintenance; resulting in delays or cancellation.</span>
       </li>
 
       <li styleName="box">
         <img src={formIcon} alt="" styleName="icon" />
         <span>Waiver and release of liability will be available on-site and must be signed before participating. For visitors ages 13 to 17, a guardian must be present to sign the waiver and release of liability at the time of the visit.</span>
+      </li>
+
+      <li styleName="box">
+        <img src={eyeIcon} alt="" styleName="icon" />
+        <span>CARNE y ARENA is not recommended for individuals with claustrophobia, heart conditions, back conditions, a history of seizures, epilepsy, and/or sensitivity to flashing lights.</span>
       </li>
     </ul>
   </div>
