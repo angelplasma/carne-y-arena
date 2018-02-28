848737622b43c53afd96214316e34f813c311143
Jeremy Frank <jeremyjfrank@gmail.com>
2018-02-28 08:46:18 -0500

    Move top padding to main element and style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from './404.module.scss'
+
 const NotFoundPage = () => (
   <div>
-    <h1>Page Not Found</h1>
+    <h1 styleName="title">Page Not Found</h1>
   </div>
 )
 
