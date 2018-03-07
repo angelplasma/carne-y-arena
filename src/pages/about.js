cc431cb14dec37f525cacc319d64a1f29ae190ef
Jeremy Frank <jeremyjfrank@gmail.com>
2018-03-07 09:37:59 -0500

    Adjust about page copy and styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,10 @@ import React from 'react'
 import { TICKETS_URL } from '../../lib/constants'
 import styles from './about.module.scss'
 
-const quoteText = `My intention was to experiment with VR technology to explore
-  the <i>human</i> condition in an attempt to break the dictatorship of
-  the frame, within which things are just <i>observed</i>`
+const partialQuoteText = `I’ve experimented with VR technology to explore the
+  <i>human</i> condition in an attempt to break the dictatorship of the
+  frame—within which things are just <i>observed</i>—and claim the space to
+  allow the visitor to go through a direct experience`
 
 const AboutPage = () => (
   <div styleName="container">
@@ -13,27 +14,28 @@ const AboutPage = () => (
 
     <div styleName="content">
       <p>
-        <b>Alejandro G. Iñárritu’s conceptual virtual reality installation CARNE
-        y ARENA</b> (Virtually present, Physically invisible) explores the human
-        condition of immigrants and refugees. Based on true accounts, the
-        superficial lines between subject and bystander are blurred and bound
-        together, allowing individuals to walk in a vast space and thoroughly
-        live a fragment of the refugees’ personal journeys. An immersive
-        installation that reunites frequent collaborators Iñárritu and Emmanuel
-        Lubezki alongside producer Mary Parent and ILMxLAB, CARNE y ARENA is
-        centered around a 6½-minute virtual reality sequence for one person
-        that employs state-of-the-art immersive technology to create a
-        multi-narrative light space with human characters.
+        <b>Academy Award®-winning director Alejandro G. Iñárritu’s conceptual
+        virtual reality installation CARNE y ARENA</b> (Virtually present,
+        Physically invisible)—meaning “flesh and sand”—explores the human
+        condition of immigrants and refugees. Based on true accounts from
+        Central American and Mexican refugees, CARNE y ARENA blurs and binds
+        together the superficial lines between subject and bystander, allowing
+        individuals to walk in a vast space and live a fragment of a refugee’s
+        personal journey. Awarded a special Oscar last year, CARNE y ARENA is a
+        six-and-a-half-minute solo experience that employs state-of-the-art
+        technology to create a multi-narrative space with human characters. The
+        project reunites frequent collaborators Iñárritu and three-time Academy
+        Award®-winning cinematographer Emmanuel Lubezki, alongside producer Mary
+        Parent and ILMxLAB.
       </p>
 
       <p>
-        “During the past four years in which this project has been growing in
-        my mind, I had the privilege of meeting and interviewing many Mexican
-        and Central American refugees. Their life stories haunted me, so I
-        invited some of them to collaborate with me in the project,” Iñárritu
-        says. “<span dangerouslySetInnerHTML={{ __html: quoteText }} />, and claim the space to allow the
-        visitor to go through a direct experience walking in the immigrants’
-        feet, under their skin, and into their hearts.”
+        “During the past five years in which this project has been growing in my
+        mind, I had the privilege of meeting and interviewing many Mexican and
+        Central American refugees. Their life stories haunted me, so I invited
+        some of them to collaborate with me in the
+        project, <span dangerouslySetInnerHTML={{ __html: partialQuoteText }} /> walking
+        in the immigrants’ feet, under their skin, and into their hearts.”
       </p>
 
       <a href={TICKETS_URL} className="button" rel="noopener">Reserve Your Space</a>
@@ -41,7 +43,7 @@ const AboutPage = () => (
 
     <aside styleName="background">
       <blockquote>
-        <p dangerouslySetInnerHTML={{ __html: `“${quoteText}.”` }} />
+        <p dangerouslySetInnerHTML={{ __html: `“${partialQuoteText}.”` }} />
         <cite>Alejandro G. Iñárrito</cite>
       </blockquote>
     </aside>
