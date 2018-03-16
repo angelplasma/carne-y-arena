c852abee9a4005e1d9a30da90927ec7bab5e2155
Jeremy Frank <jeremyjfrank@gmail.com>
2018-03-16 08:48:47 -0400

    Add container class to 404 page and remove max-width

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './404.module.scss'
 
 const NotFoundPage = () => (
-  <div>
+  <div className="container">
     <h1 styleName="title">Page Not Found</h1>
   </div>
 )
