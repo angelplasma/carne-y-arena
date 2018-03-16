9d76d72bc0cbcf68ab6c2311d7d2e65031ad1daa
Jeremy Frank <jeremyjfrank@gmail.com>
2018-03-16 08:49:04 -0400

    Add contact page and adjust container styles

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ContactPage = () => (
+  <div className="container">
+    <h1>Contact</h1>
+    <p>Email: <a href="mailto:info@carneyarenadc.com">info@carneyarenadc.com</a></p>
+  </div>
+)
+
+export default ContactPage
